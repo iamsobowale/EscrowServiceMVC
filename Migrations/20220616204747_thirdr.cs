3a1f93e5bae82f731d3ca20def83154f9e306171
iamsobowale <iamsobowale>
2022-07-26 09:44:55 +0100

    u

---
+++ Migrations/20220616204747_thirdr.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscrowService.Migrations
+{
+    public partial class thirdr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaidOut",
+                table: "TransactionTypes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaidOut",
+                table: "TransactionTypes",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
---
+++ Migrations/20220715185055_tjj.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscrowService.Migrations
+{
+    public partial class tjj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BankCode",
+                table: "Traders",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankCode",
+                table: "Traders");
+        }
+    }
+}
